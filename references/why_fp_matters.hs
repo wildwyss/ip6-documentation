d0242e24ad02578b055dbb80f1a206ee312e669b
Andri Wild <andri.wild@students.fhnw.ch>
2023-06-15 09:26:03 +0200

    add some haskell functions to why_fp_matters.hs

--- references/why_fp_matters.hs
+++ references/why_fp_matters.hs
@@ -1,5 +1,6 @@
 import Data.Tree (Tree(rootLabel))
 import Prelude hiding (repeat)
+
 main :: IO ()
 main = do
   print "helo"
@@ -79,3 +80,53 @@ integrate :: Fractional a => (a -> a) -> a -> a -> [a]
 integrate f a b = cons (easyintegrate f a b)
                        (zipWith (+) (integrate f a mid) (integrate f mid b))
                         where mid = (a + b)/2
+
+-- AI
+
+newtype Position = Position Int
+
+moves :: Position -> [Position]
+moves (Position p) = [Position (p-1), Position p, Position (p+1)]
+
+reptree :: (a -> [a]) -> a -> TreeOf a
+reptree f a = Node a (map (reptree f) (f a))
+
+gametree :: Position -> TreeOf Position
+gametree = reptree moves
+
+static :: Position -> Int
+static (Position p) = p
+
+maximize :: TreeOf Int -> Int
+maximize (Node n []) = n
+maximize (Node n sub) = maximum (map minimize sub)
+
+minimize:: TreeOf Int -> Int
+minimize (Node n []) = n
+minimize (Node n sub) = minimum (map maximize sub)
+
+prune :: Int -> TreeOf a -> TreeOf a
+prune 0 (Node a _) = Node a nil
+prune n (Node a sub) = Node a (map (prune (n-1)) sub)
+
+
+omit :: Int -> [[Int]] -> [Int]
+omit pot (nums:rest) | minleq nums pot = omit pot rest
+                     | otherwise = cons (minimum nums) (omit (minimum nums) rest)
+
+minleq :: [Int] -> Int -> Bool
+minleq [] _ = False
+minleq (n:rest) pot | n <= pot = True
+                    | otherwise = minleq rest pot
+
+maximize' :: TreeOf Int -> [Int]
+maximize' (Node n []) = cons n []
+maximize' (Node _ l) = mapmin (map minimize' l)
+  where mapmin (nums:rest) = cons (minimum nums) (omit (minimum nums) rest) 
+
+minimize' :: TreeOf Int -> [Int]
+minimize' (Node n []) = cons n []
+minimize' (Node _ l) = mapmax (map maximize' l)
+  where mapmax (nums:rest) = cons (maximum nums) (omit (maximum nums) rest) 
+
+evaluate = max . maximize' . mapTree static . prune 6 . gametree
